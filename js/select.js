9054735723189b27c11fe1e840886d657785958c
Renan Ferreira <renanfc.re@gmail.com>
2023-05-14 19:21:56 -0300

    Correcao de funcao de selecao de tema

--- js/select.js
+++ js/select.js
@@ -1,5 +1,6 @@
 document.addEventListener('click', (btn) => {
     const el = btn.target;
-    if (el.classList.contains('btn-theme')) window.location = 'pages/game.html';
+    if (el.classList.contains('btn-theme')) window.location = 'pages/game.html';   
 
+    localStorage.setItem('theme', el.value)
 });
